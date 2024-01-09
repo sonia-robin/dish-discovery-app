b9a3f5e1e57302fbfa9accd76247d06363ab723c
unknown <cervinkova.sonik@gmail.com>
2024-01-09 14:38:58 +0000

    Display title, image, ingredients and instructions in recipe page

--- assets/js/recipe-page.js
+++ assets/js/recipe-page.js
@@ -6,7 +6,7 @@ var apiKeyF = "8f932f55cd354bf89c0d697bb2662998";
 
 console.log(recipeIdUrl);
 
-var queryUrlRecipeInfo = "https://api.spoonacular.com/recipes/" + recipeIdUrl + "/information?includeNutrition=false&apiKey=" + apiKeySona;
+var queryUrlRecipeInfo = "https://api.spoonacular.com/recipes/" + recipeIdUrl + "/information?includeNutrition=false&apiKey=" + apiKeyF;
 
 fetch(queryUrlRecipeInfo)
     .then(function(response) {
@@ -16,25 +16,44 @@ fetch(queryUrlRecipeInfo)
         console.log(data);
         var recipeTitle = data.title;
         var recipeImg = data.image;
+        document.getElementById("recipe-title").textContent = recipeTitle;
+        // var recipeImgEl = document.createElement("img");
+        // recipeImgEl.setAttribute("src", recipeImg);
+        // recipeImgEl.setAttribute("alt", "recipe-image");
+        // recipeImgEl.setAttribute("class", "w-100")
+        // document.getElementById("image").appendChild(recipeImgEl);
+        var card = document.createElement("div");
+        card.setAttribute("class", "card w-100");
+        document.getElementById("image").appendChild(card);
+        var recipeImgEl = document.createElement("img");
+        recipeImgEl.setAttribute("src", recipeImg);
+        recipeImgEl.setAttribute("alt", "recipe-image");
+        recipeImgEl.setAttribute("class", "card-img-top custom-width-recipe");
+        card.style.overflow = "hidden";
+        card.appendChild(recipeImgEl);
+        document.getElementById("ingredients")
+        document.getElementById("instructions")
+    
         var cookingTime = data.readyInMinutes;
         var servings = data.servings;
+
+        document.getElementById("ingr-header").textContent = "Ingredients";
+        document.getElementById("instr-header").textContent = "Instructions";
         for (var i = 0; i < data.extendedIngredients.length; i++){
-            console.log(data.extendedIngredients[i].original)
+            var ingredient = data.extendedIngredients[i].original
+            var li = document.createElement("li");
+            li.textContent = ingredient;
+            document.getElementById("ingr-list").appendChild(li);
+
         }
 
         for (var i = 0; i < data.analyzedInstructions[0].steps.length; i++){
-            console.log(data.analyzedInstructions[0].steps[i].step)
+            var step = data.analyzedInstructions[0].steps[i].step;
+            var li = document.createElement("li");
+            li.textContent = step;
+            document.getElementById("instr-list").appendChild(li);
         }
         
-
-        // var recipeInstructions = data.instructions;
-        // console.log(recipeInstructions);
-        // var parser = new DOMParser();
-        // var document = parser.parseFromString(recipeInstructions, "text/html");
-        // console.log(document);
-        // var content = document.querySelector("ol").innerHTML;
-        // recipeSection.innerHTML = content;
-
     })
     .catch(function(error) {
         console.error('Error fetching data:', error);
