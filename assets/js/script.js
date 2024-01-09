019c528ee4fa4cb4e5f36a69071e089c9feb8e50
unknown <cervinkova.sonik@gmail.com>
2024-01-09 20:46:00 +0000

    Implemented YouTube function in the main js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,10 @@ var apiKey = "08cce16bdc354b6aa373715c0843bfc8";
 var apiKeySona = "f197b6604aa242d3bf1aa8e74d25e259";
 var apiKeyS = "5f91a7deb5df4f2db6a1138caa6d3e5f";
 var apiKeyF = "8f932f55cd354bf89c0d697bb2662998";
+var apiKeyYT = "AIzaSyBz2fXaCNuTho3B_k-OLJpoMlLzrnw7KfA";
 var recipeListContainer = document.getElementById('spoonacular-api');
 console.log(recipeListContainer);
+var youTubeDiv = document.querySelector("#youtube-api");
 
 
 // Add event listener when form submitted
@@ -30,11 +32,12 @@ document.getElementById('recipe-form').addEventListener('submit', function (even
     console.log(allergies);
     // Call fetch recipe function, diet and allergies pass as parameters
     fetchRecipeId(searchInput, diet, allergies, apiKeyS);
+    displayYouTubeResults(searchInput, diet, apiKeyYT);
     });
 
 // Function to search by user input, diet and allergies and get recipe ID
 function fetchRecipeId (searchPar, dietPar, allergiesPar, apiKeyPar){
-    var queryUrl = "https://api.spoonacular.com/recipes/complexSearch?query=" + searchPar + "&number=1&diet=" + dietPar + "&intolerances=" + allergiesPar + "&instructionsRequired=true&apiKey=" + apiKeyPar;
+    var queryUrl = "https://api.spoonacular.com/recipes/complexSearch?query=" + searchPar + "&number=6&diet=" + dietPar + "&intolerances=" + allergiesPar + "&instructionsRequired=true&apiKey=" + apiKeyPar;
     console.log(queryUrl);
     var recipeId = [];
     fetch(queryUrl)
@@ -117,6 +120,64 @@ function displayRecipe (idsPar, apiKeyPar){
 }
 
 // Function do display YouTube results
+function displayYouTubeResults (searchPar, dietPar, apiKeyPar){
+    var baseSearchUrl = "https://www.youtube.com/watch?v=";
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=6&q=" + searchPar + "+" + dietPar + "+recipe&key=" + apiKeyPar;
+    
+    fetch(queryURL)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+  
+    console.log(data);
+    // empty YouTube section first
+    youTubeDiv.textContent = "";
+    // retrieve video ID (needed to create a link), title, thumbnail
+    for (var i = 0; i < data.items.length; i++){
+    var videoId = data.items[i].id.videoId;
+    var titleVideo = replaceHtmlEntity(data.items[i].snippet.title);
+    var thumbnailImg = data.items[i].snippet.thumbnails.medium.url;
+    // create column div
+    var colDiv = document.createElement("div");
+    colDiv.setAttribute("class", "col");
+    youTubeDiv.appendChild(colDiv)
+    // create card div element which is a link to YouTube video and set bootsrap class card, append
+    var cardDiv = document.createElement("a");
+    cardDiv.setAttribute("href", baseSearchUrl + videoId);
+    cardDiv.setAttribute("target", "blank");
+    cardDiv.setAttribute("class", "card h-100 link-underline link-underline-opacity-0 p-0");
+    colDiv.appendChild(cardDiv);
+    // create img element and set class and src, append to card div
+    var imgIconEl = document.createElement("div");
+    imgIconEl.setAttribute("class", "center-container");
+    cardDiv.appendChild(imgIconEl);
+    var imgEl = document.createElement("img");
+    imgEl.setAttribute("class", "card-img-top");
+    imgEl.setAttribute("src", thumbnailImg);
+    imgEl.setAttribute("alt", "thumbnail");
+    imgIconEl.appendChild(imgEl);
+    var iconEl = document.createElement("div");
+    iconEl.setAttribute("class", "icon-overlay");
+    imgIconEl.appendChild(iconEl);
+    var icon = document.createElement("i");
+    icon.setAttribute("class", "fa fa-youtube-play");
+    iconEl.appendChild(icon);
 
+    // create card body el, set class, append to card div
+    var cardBody = document.createElement("div");
+    cardBody.setAttribute("class", "card-body");
+    cardDiv.append(cardBody);
+    // create title el
+    var cardTitle = document.createElement("h6");
+    cardTitle.setAttribute("class", "card-title text-center");
+    cardTitle.textContent = titleVideo;
+    cardBody.appendChild(cardTitle);
+    }  
+    })
+    }
 
-
+// Function to replace HTML entities in a string for special characters
+function replaceHtmlEntity(str){
+    return str.replace(/&amp;/g, "&").replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&#39;/g, "'");
+  }
