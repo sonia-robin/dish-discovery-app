8d38f4f067fc48213cf5d10e196586fbe4304728
unknown <cervinkova.sonik@gmail.com>
2024-01-10 18:44:20 +0000

    changed all allergies to cuisine

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Global variables
 var apiKey = "08cce16bdc354b6aa373715c0843bfc8";
-var apiKeySona = "f197b6604aa242d3bf1aa8e74d25e259";
-var apiKeyS = "5f91a7deb5df4f2db6a1138caa6d3e5f";
+var apiKeyFona = "f197b6604aa242d3bf1aa8e74d25e259";
+var apiKeyF = "5f91a7deb5df4f2db6a1138caa6d3e5f";
 var apiKeyF = "8f932f55cd354bf89c0d697bb2662998";
 var apiKeyYT = "AIzaSyBz2fXaCNuTho3B_k-OLJpoMlLzrnw7KfA";
 var recipeListContainer = document.getElementById("spoonacular-api");
@@ -14,38 +14,38 @@ document
     event.preventDefault();
     var searchInput = document.getElementById("keyword").value;
     var diet = [];
-    var allergies = [];
-    // Select all checked dietary and allergies requirements
+    var cuisine = [];
+    // Select all checked dietary and cuisine requirements
     var checkedDietary = document.querySelectorAll(
       "input[type='checkbox'][name='dietary']:checked"
     );
-    var checkedAllergies = document.querySelectorAll(
-      "input[type='checkbox'][name='allergies']:checked"
+    var checkedCuisine = document.querySelectorAll(
+      "input[type='checkbox'][name='cuisine']:checked"
     );
-    // Dietary and allergies requirements into 2 arrays
+    // Dietary and cuisine requirements into 2 arrays
     checkedDietary.forEach(function (checkbox) {
       diet.push(checkbox.value);
     });
-    checkedAllergies.forEach(function (checkbox) {
+    checkedCuisine.forEach(function (checkbox) {
       var labelFor = checkbox.getAttribute("id");
       if (labelFor) {
-        allergies.push(labelFor);
+        cuisine.push(labelFor);
       }
     });
-    // Call fetch recipe function, diet and allergies pass as parameters
-    fetchRecipeId(searchInput, diet, allergies, apiKeyS);
-    displayYouTubeResults(searchInput, diet, allergies, apiKeyYT);
+    // Call fetch recipe function, diet and cuisine pass as parameters
+    fetchRecipeId(searchInput, diet, cuisine, apiKeyF);
+    displayYouTubeResults(searchInput, diet, cuisine, apiKeyYT);
   });
 
-// Function to search by user input, diet and allergies and get recipe ID
-function fetchRecipeId(searchPar, dietPar, allergiesPar, apiKeyPar) {
+// Function to search by user input, diet and cuisine and get recipe ID
+function fetchRecipeId(searchPar, dietPar, cuisinePar, apiKeyPar) {
   var queryUrl =
     "https://api.spoonacular.com/recipes/complexSearch?query=" +
     searchPar +
     "&number=1&diet=" +
     dietPar +
     "&intolerances=" +
-    allergiesPar +
+    cuisinePar +
     "&instructionsRequired=true&apiKey=" +
     apiKeyPar;
   var recipeId = [];
@@ -61,7 +61,7 @@ function fetchRecipeId(searchPar, dietPar, allergiesPar, apiKeyPar) {
           recipeId.push(data.results[i].id);
         }
         // Call display recipe function, pass recipe IDs as parameter
-        displayRecipe(recipeId, apiKeyS);
+        displayRecipe(recipeId, apiKeyF);
         // If no results found, display an error
       } else {
         recipeListContainer.innerHTML = "No recipes found.";
@@ -161,7 +161,7 @@ function displayRecipe(idsPar, apiKeyPar) {
 }
 
 // Function to display YouTube results
-function displayYouTubeResults(searchPar, dietPar, allergiesPar, apiKeyPar) {
+function displayYouTubeResults(searchPar, dietPar, cuisinePar, apiKeyPar) {
   var baseSearchUrl = "https://www.youtube.com/watch?v=";
   var queryURL =
     "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" +
@@ -179,7 +179,7 @@ function displayYouTubeResults(searchPar, dietPar, allergiesPar, apiKeyPar) {
       console.log(data);
       // empty YouTube section first
       youTubeDiv.textContent = "";
-      if (allergiesPar.length == 0) {
+      
         // retrieve video ID (needed to create a link), title, thumbnail
         for (var i = 0; i < data.items.length; i++) {
           var videoId = data.items[i].id.videoId;
@@ -224,7 +224,7 @@ function displayYouTubeResults(searchPar, dietPar, allergiesPar, apiKeyPar) {
           cardTitle.setAttribute("class", "card-title text-center");
           cardTitle.textContent = titleVideo;
           cardBody.appendChild(cardTitle);
-        }
+        
       }
     });
 }
